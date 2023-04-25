2c86007f2ba7fabf86126f4d0aa8b45bca39f640
nabuco <nabucopena@gmail.com>
2023-04-25 11:55:52 -0300

    First commit
    I forgot to commit before
    This app has 3 screens
      - Home screen
      - Profile (empty by now)
      - Location (it shows your coordinates)

---
+++ App.tsx
@@ -0,0 +1,151 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, Text, Button, View, PermissionsAndroid} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Geolocation from 'react-native-geolocation-service';
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+    <Text>
+      "Home Screen"
+    </Text>
+    <Button
+      title="Profile"
+      onPress={() =>
+        navigation.navigate("Profile", {name: "Nabuco"})
+      }
+    />
+    <Button
+          title="Location"
+          onPress={() =>
+            navigation.navigate("Location")
+          }
+        />
+    </View>
+  );
+}
+
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+
+
+{/* Location */}
+
+
+
+
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+
+
+const Location = ({navigation}) => {
+
+  const [location, setLocation] = useState(false);
+
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      console.log('res is:', res);
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    });
+    console.log(location);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome!</Text>
+      <View
+        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+        <Button title="Get Location" onPress={getLocation} />
+      </View>
+      <Text>Latitude: {location ? location.coords.latitude : null}</Text>
+      <Text>Longitude: {location ? location.coords.longitude : null}</Text>
+      <View
+        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+        <Button title="Send Location" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'center',
+   justifyContent: 'center',
+ },
+});
+
+
+
+
+const Nav = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Location" component={Location} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+export default Nav
